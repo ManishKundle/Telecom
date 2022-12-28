499e16193454aeecb75ba05e8c6db7674a523d97
ManishKundle <manishkundle5@gmail.com>
2022-12-28 17:36:54 +0530

    Hashmap code added

--- src/main/java/com/telecom/Telecom/controller/LoginController.java
+++ src/main/java/com/telecom/Telecom/controller/LoginController.java
@@ -1,11 +1,17 @@
 package com.telecom.Telecom.controller;
 
+import java.util.HashMap;
+
 public class LoginController {
 
-	
 	public static void main(String[] args) {
-		
-		
-		
+
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+		map.put("ram", 500);
+		map.put("rohit", 1000);
+		map.put("manoj", 1500);
+
+		map.forEach((k, v) -> System.out.println("Key = " + k + " Value = " + v));
 	}
 }
